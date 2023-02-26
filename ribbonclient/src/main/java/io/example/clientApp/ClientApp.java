3ab19c765793078cbd27ff5d504b8d1a0408d0da
Alex Franco <26696700+afranco20@users.noreply.github.com>
2023-02-26 17:14:27 -0500

    :fire: remove RibbonConfiguration.java

--- ribbonclient/src/main/java/io/example/clientApp/ClientApp.java
+++ ribbonclient/src/main/java/io/example/clientApp/ClientApp.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
+@RibbonClient(name = "server")
 public class ClientApp {
     public static void main(String[] args) {
         SpringApplication.run(ClientApp.class, args);
