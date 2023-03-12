e225734487f8360eb5c5261bb74f4f8d3b8e1d1e
Alex Franco <26696700+afranco20@users.noreply.github.com>
2023-03-12 01:38:21 -0500

    :arrow_up: replace netflix ribbon with spring cloud loadbalancer

--- ribbonclient/src/main/java/io/example/clientApp/ClientApp.java
+++ ribbonclient/src/main/java/io/example/clientApp/ClientApp.java
@@ -3,11 +3,11 @@ package io.example.clientApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RibbonClient(name = "server")
+@LoadBalancerClient(name = "server")
 public class ClientApp {
     public static void main(String[] args) {
         SpringApplication.run(ClientApp.class, args);
